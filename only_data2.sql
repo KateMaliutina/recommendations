c50a89fc49fc9c81581599fed1316d2f5be87fed
ekaterinamalyutina <malyutina.kate@gmail.com>
2025-05-04 15:01:32 +0300

    new data

---
+++ only_data2.sql
@@ -0,0 +1,579 @@
+
+COPY public.roadmap_nodes (id, roadmap_id, skill_id, parent_id, is_optional) FROM stdin;
+1	7	53	\N	f
+2	7	54	1	f
+3	7	55	1	f
+4	7	56	\N	f
+5	7	5	4	t
+6	7	8	4	t
+7	7	41	4	t
+8	7	44	4	t
+9	7	57	4	t
+10	7	58	4	t
+11	7	59	4	t
+12	7	60	4	t
+13	7	61	\N	f
+14	7	10	13	f
+15	7	20	13	t
+16	7	62	13	t
+17	7	95	\N	f
+18	7	63	17	t
+19	7	64	17	t
+20	7	43	17	t
+21	7	39	\N	f
+22	7	65	21	f
+23	7	66	21	f
+24	7	67	21	f
+25	7	68	21	f
+26	7	69	21	f
+27	7	70	21	t
+28	7	71	21	t
+29	7	72	21	t
+30	7	73	21	t
+31	7	75	\N	f
+32	7	74	31	t
+33	7	48	31	f
+34	7	76	31	t
+35	7	77	31	t
+36	7	78	\N	t
+37	7	79	36	t
+38	7	80	36	t
+39	7	81	36	t
+40	7	101	36	t
+41	7	102	36	t
+42	7	103	36	t
+43	7	104	36	t
+44	7	82	\N	f
+45	7	83	44	t
+46	7	84	44	f
+47	7	85	44	f
+48	7	86	\N	f
+49	7	87	48	f
+50	7	88	48	t
+51	7	133	\N	t
+52	7	89	51	f
+53	7	90	51	f
+54	7	91	51	f
+55	7	92	51	f
+56	7	93	51	f
+57	7	94	51	t
+58	7	96	51	f
+59	7	100	\N	t
+60	7	97	59	t
+61	7	98	59	t
+62	7	99	59	t
+63	7	134	\N	f
+64	7	49	63	f
+65	7	50	63	t
+66	7	51	63	t
+67	7	105	\N	f
+68	7	106	67	f
+69	7	107	67	f
+70	7	108	67	f
+71	7	109	67	f
+72	7	110	\N	f
+73	7	111	72	t
+74	7	112	72	t
+75	7	47	72	t
+76	7	131	\N	t
+77	7	113	76	t
+78	7	114	76	t
+79	7	115	76	t
+80	7	116	76	t
+81	7	117	\N	f
+82	7	118	81	f
+83	7	119	81	t
+84	7	120	\N	f
+85	7	121	84	f
+86	7	122	\N	f
+87	7	123	86	t
+88	7	124	86	t
+89	7	125	86	t
+90	7	48	86	t
+91	7	135	\N	t
+92	7	126	91	t
+93	7	127	91	t
+94	7	128	91	t
+95	7	129	91	t
+96	7	130	91	t
+97	7	132	91	t
+98	9	53	\N	f
+99	9	54	98	f
+100	9	55	98	f
+101	9	56	\N	f
+102	9	5	101	t
+103	9	8	101	f
+104	9	23	101	f
+105	9	38	101	f
+106	9	136	103	f
+107	9	61	\N	f
+108	9	10	107	f
+109	9	20	107	t
+110	9	62	107	t
+111	9	137	\N	f
+112	9	138	111	f
+113	9	139	111	f
+114	9	140	\N	f
+115	9	25	114	t
+116	9	3	114	t
+117	9	7	114	t
+118	9	141	114	t
+119	9	142	114	t
+120	9	21	105	t
+121	9	26	105	f
+122	9	15	103	t
+123	9	16	103	t
+124	9	24	103	t
+125	9	30	103	t
+126	9	143	\N	f
+127	9	149	126	f
+128	9	144	127	f
+129	9	145	127	f
+130	9	146	126	f
+131	9	147	130	f
+132	9	148	130	f
+133	9	33	130	t
+134	9	82	\N	f
+135	9	150	134	f
+136	9	151	134	t
+137	9	152	134	f
+138	9	153	134	t
+139	9	39	\N	f
+140	9	65	139	f
+141	9	66	139	f
+142	9	67	139	f
+143	9	68	139	f
+144	9	69	139	f
+145	9	70	139	t
+146	9	71	139	t
+147	9	72	139	t
+148	9	73	139	t
+149	9	78	\N	f
+150	9	79	149	t
+151	9	80	149	t
+152	9	81	149	t
+153	9	101	149	f
+154	9	102	149	f
+155	9	103	149	f
+156	9	104	149	t
+157	9	154	\N	t
+158	9	155	114	t
+159	5	56	\N	f
+160	5	156	159	f
+161	5	59	159	t
+162	5	159	\N	f
+163	5	157	162	t
+164	5	158	162	f
+165	5	12	162	t
+166	5	61	\N	f
+167	5	10	166	f
+168	5	20	166	t
+169	5	62	166	t
+170	5	160	\N	f
+171	5	161	170	f
+172	5	162	171	f
+173	5	163	171	t
+174	5	164	171	t
+175	5	165	170	f
+176	5	166	170	f
+177	5	167	176	f
+178	5	168	176	f
+179	5	169	176	f
+180	5	170	170	f
+181	5	171	170	f
+182	5	172	170	f
+183	5	173	\N	f
+184	5	174	183	f
+185	5	73	183	t
+186	5	175	183	f
+187	5	176	183	f
+188	5	177	186	f
+189	5	178	186	f
+190	5	179	186	t
+191	5	180	186	f
+192	5	181	186	f
+193	5	182	187	f
+194	5	183	187	f
+195	5	184	187	f
+196	5	185	187	f
+197	5	186	187	f
+198	5	187	187	f
+199	5	188	187	f
+200	5	189	187	f
+201	5	190	187	f
+202	5	191	187	f
+203	5	192	187	f
+204	5	165	187	f
+205	5	105	\N	f
+206	5	193	205	f
+207	5	194	205	f
+208	5	195	205	t
+209	5	196	205	t
+210	5	157	205	f
+211	5	206	210	f
+212	5	197	211	f
+213	5	198	211	t
+214	5	199	211	t
+215	5	200	211	f
+216	5	201	205	f
+217	5	202	216	f
+218	5	203	216	f
+219	5	204	216	f
+220	5	205	216	t
+221	5	207	\N	f
+222	5	208	221	f
+223	5	209	221	f
+224	5	210	221	f
+225	5	211	221	f
+226	5	212	221	f
+227	5	53	\N	f
+228	5	213	227	f
+229	5	214	227	f
+230	5	215	227	f
+231	5	216	227	f
+232	5	198	227	t
+233	5	199	227	t
+234	5	217	227	f
+235	5	125	\N	f
+236	5	149	\N	t
+237	5	218	236	t
+238	5	219	236	t
+239	5	82	\N	f
+240	5	220	239	f
+241	5	221	239	f
+242	5	143	\N	f
+243	5	222	242	f
+244	5	223	242	t
+245	5	224	243	t
+246	5	225	243	f
+247	5	226	243	f
+\.
+
+
+--
+-- Data for Name: roadmaps; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.roadmaps (id, specialization, grade) FROM stdin;
+1	qa	intern
+2	qa	junior
+3	ios	intern
+4	ios	junior
+5	android	intern
+6	android	junior
+7	backend	intern
+8	backend	junior
+9	frontend	intern
+10	frontend	junior
+11	fullstack	intern
+12	fullstack	junior
+13	product manager	intern
+14	product manager	junior
+15	project manager	intern
+16	project manager	junior
+17	game developer	intern
+18	game developer	junior
+19	devops	intern
+20	devops	junior
+21	ux/ui designer	intern
+22	ux/ui designer	junior
+23	data scientist	intern
+24	data scientist	junior
+\.
+
+
+--
+-- Data for Name: skills; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.skills (id, name) FROM stdin;
+3	react
+4	redux
+5	typescript
+7	vue
+8	javascript
+10	git
+12	алгоритмы и структуры данных
+13	графы
+14	граф
+15	d3.js
+16	three.js
+17	networkx
+18	graph-tool
+19	pygraphvis
+20	github
+21	sass
+22	es6
+23	html
+24	zustand
+25	nuxt
+27	strapi
+28	cms wordpress
+29	modx
+30	ext js
+31	работа с большим объемом информации
+32	деловая переписка
+33	webpack
+34	решение проблем
+35	ответственность
+36	грамотная устная речь
+37	пользователь пк
+38	css
+39	api
+40	gulp
+41	c#
+42	asp.net
+43	postgresql
+44	php
+45	yii2
+46	rest api
+47	rabbitmq
+48	redis
+49	docker
+50	kubernetes
+51	helm
+52	laravel
+53	интернет
+54	http
+55	dns
+56	языки программирования
+57	go
+58	python
+59	java
+60	rust
+61	системы контроля версий
+62	gitlab
+63	sqlite
+64	mysql
+65	jwt
+66	oauth
+67	cookie
+68	rest
+69	json
+70	grpc
+71	graphql
+72	soap
+73	xml
+74	memcached
+75	кэширование
+76	cdn
+77	кэширование на стороне клиента
+78	безопасность
+79	sha
+80	bcrypt
+81	md5
+82	тестирование
+83	интеграционное
+84	unit
+85	функциональное
+86	ci/cd
+87	github actions
+88	jenkins
+89	orm
+90	acid
+91	транзакции
+92	нормализация
+93	миграции
+94	n+1 проблема
+95	базы данных
+96	индексы
+97	репликация
+98	шардирование
+99	cap теорема
+100	масштабирование
+101	https
+102	cors
+103	ssl/tls
+104	sql инъекции
+105	архитектурные паттерны
+106	монолит
+107	микросервисы
+108	stateless
+109	statefull
+110	брокеры сообщений
+111	kafka
+112	nats
+113	elk стек
+114	elasticsearch
+115	grafana
+116	kibana
+117	балансировщики нагрузки
+118	nginx
+119	haproxy
+120	общение в режиме реального времени
+121	websockets
+122	nosql базы данных
+123	mongodb
+124	cassandra
+125	firebase
+126	graceful degradation
+127	throttling
+128	loadshifting
+129	backpressure
+130	circuit breaker
+131	observability
+132	мониторинг
+133	базы данных углубленно
+134	контейнеризация и виртуализация
+135	дальнейшее масштабирование
+136	dom
+137	пакетные менеджеры
+138	yarn
+139	npm
+140	framework
+141	angular
+142	next
+26	tailwind
+143	средства сборки
+144	prettier
+145	eslint
+146	module bundlers
+147	vite
+148	esbuild
+149	линтеры
+150	vitest
+151	jest
+152	playwright
+153	cypress
+154	pwa
+155	astro
+156	kotlin
+157	ооп
+158	android studio
+159	основы
+160	компоненты приложения
+161	activity
+162	activity lifecycle
+163	изменения state
+164	task
+165	fragment
+166	intent
+167	явные
+168	неявные
+169	intent filter
+170	services
+171	content provider
+172	broadcast receiver
+173	interface
+174	jetpack compose
+175	layout
+176	элементы
+177	frame
+178	linear
+179	relative
+180	constraint
+181	recycleview
+182	textview
+183	toast
+184	dialog
+185	button
+186	bottom sheet
+187	imageview
+188	tab
+189	edittext
+190	drawer
+191	listview
+192	animations
+193	mvi
+194	mvvm
+195	mvp
+196	mvc
+197	flow
+198	rxkotlin
+199	rxjava
+200	livedata
+201	работа с зависимостями
+202	dagger
+203	hilt
+204	koin
+205	kodein
+206	наблюдатель
+207	локальное хранение
+208	shared preferences
+209	encrypted shared preferences
+210	datastore
+211	room database
+212	file system
+213	retrofit
+214	okhttp
+215	coroutines
+216	threads
+217	workmanager
+218	ktlint
+219	detekt
+220	espresso
+221	junit
+222	gradle
+223	maven
+224	gradle kotlin dsl
+225	build.gradle.kts
+226	settings.gradle.kts
+\.
+
+
+--
+-- Data for Name: user_roadmap_nodes; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.user_roadmap_nodes (id, user_id, roadmap_node_id, status) FROM stdin;
+\.
+
+
+--
+-- Data for Name: user_skills; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.user_skills (user_id, skill_id, proficiency_level) FROM stdin;
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (id, name, email, interests, grade) FROM stdin;
+\.
+
+
+--
+-- Data for Name: vacancies; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.vacancies (id, title, description, grade, employer_name, url) FROM stdin;
+\.
+
+
+--
+-- Data for Name: vacancy_skills; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.vacancy_skills (vacancy_id, skill_id) FROM stdin;
+\.
+
+
+--
+-- Name: recommendations_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.recommendations_id_seq', 1, false);
+
+
+--
+-- Name: roadmap_nodes_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.roadmap_nodes_id_seq', 247, true);
+
+
+--
+-- Name: roadmaps_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.roadmaps_id_seq', 24, true);
+
+
+--
+-- Name: skills_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.skills_id_seq', 226, true);
